03a661425c4e9f7dcda4d0b21519cb99d03240ad
npnquang <nguyenjoe0201@gmail.com>
2025-05-25 16:47:32 +1000

    conf: Change variable names to camelCase

--- src/lib/stores/bookmarks.ts
+++ src/lib/stores/bookmarks.ts
@@ -1,38 +1,45 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
 export type Bookmark = {
-  repo_name: string;
-  repo_url: string;
+  repoName: string;
+  repoUrl: string;
 };
 
 function createBookmarks() {
   const { subscribe, set, update } = writable<Bookmark[]>([
-    { repo_name: "fit3170-A1", repo_url: "https://github.com/user/fit3170-A1.git" },
-    { repo_name: "this-is-a-repo", repo_url: "https://gitlab.com/abc0012/this-is-a-repo.git" },
-    { repo_name: "project", repo_url: "https://github.com/example-org/project.git" },
+    { repoName: "fit3170-A1", repoUrl: "https://github.com/user/fit3170-A1.git" },
+    { repoName: "this-is-a-repo", repoUrl: "https://gitlab.com/abc0012/this-is-a-repo.git" },
+    { repoName: "project", repoUrl: "https://github.com/example-org/project.git" },
   ]);
 
   return {
     subscribe,
     add: (bookmark: Bookmark) =>
       update(bookmarks => {
-        if (!bookmarks.find(b => b.repo_url === bookmark.repo_url)) {
+        if (!bookmarks.find(b => b.repoUrl === bookmark.repoUrl)) {
           return [...bookmarks, bookmark];
         }
         return bookmarks;
       }),
+
     remove: (repo_url: string) =>
-      update(bookmarks => bookmarks.filter(b => b.repo_url !== repo_url)),
+      update(bookmarks => bookmarks.filter(b => b.repoUrl !== repo_url)),
+
     toggle: (bookmark: Bookmark) =>
       update(bookmarks => {
-        const exists = bookmarks.find(b => b.repo_url === bookmark.repo_url);
+        const exists = bookmarks.find(b => b.repoUrl === bookmark.repoUrl);
         if (exists) {
-          return bookmarks.filter(b => b.repo_url !== bookmark.repo_url);
+          return bookmarks.filter(b => b.repoUrl !== bookmark.repoUrl);
         } else {
           return [...bookmarks, bookmark];
         }
       }),
+
     clear: () => set([]),
+
+    contains: (repo_url: string): boolean => {
+      return get({ subscribe }).some(b => b.repoUrl === repo_url)
+    }
   };
 }
 
