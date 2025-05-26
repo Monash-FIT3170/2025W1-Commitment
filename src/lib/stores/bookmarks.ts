5500f14ca014c5ab2cd456965c92e839fe7b36c0
npnquang <nguyenjoe0201@gmail.com>
2025-05-26 20:50:57 +1000

    feat: Update bookmark.ts
    Bookmark type is moved to Repo.ts file and changed to Repo type

--- src/lib/stores/bookmarks.ts
+++ src/lib/stores/bookmarks.ts
@@ -1,20 +1,16 @@
 import { writable, get } from 'svelte/store';
-
-export type Bookmark = {
-  repoName: string;
-  repoUrl: string;
-};
+import type { Repo } from '$lib/repo';
 
 function createBookmarks() {
-  const { subscribe, set, update } = writable<Bookmark[]>([
-    { repoName: "fit3170-A1", repoUrl: "https://github.com/user/fit3170-A1.git" },
-    { repoName: "this-is-a-repo", repoUrl: "https://gitlab.com/abc0012/this-is-a-repo.git" },
-    { repoName: "project", repoUrl: "https://github.com/example-org/project.git" },
+  const { subscribe, set, update } = writable<Repo[]>([
+    { repoPath: "fit3170-A1", repoUrl: "https://github.com/user/fit3170-A1.git", repoType: "github" },
+    { repoPath: "this-is-a-repo", repoUrl: "https://gitlab.com/abc0012/this-is-a-repo.git", repoType: "github" },
+    { repoPath: "project", repoUrl: "https://github.com/example-org/project.git", repoType: "github" },
   ]);
 
   return {
     subscribe,
-    add: (bookmark: Bookmark) =>
+    add: (bookmark: Repo) =>
       update(bookmarks => {
         if (!bookmarks.find(b => b.repoUrl === bookmark.repoUrl)) {
           return [...bookmarks, bookmark];
@@ -25,7 +21,7 @@ function createBookmarks() {
     remove: (repo_url: string) =>
       update(bookmarks => bookmarks.filter(b => b.repoUrl !== repo_url)),
 
-    toggle: (bookmark: Bookmark) =>
+    toggle: (bookmark: Repo) =>
       update(bookmarks => {
         const exists = bookmarks.find(b => b.repoUrl === bookmark.repoUrl);
         if (exists) {
