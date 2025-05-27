00b534f3de0c8c6d6161122376e90a9d1c6d418e
Audrey Phommasone <apho0008@student.monash.edu>
2025-05-27 19:54:25 +1000

    feat: Add Avatar component

---
+++ src/lib/components/global/avatar.svelte
@@ -0,0 +1,34 @@
+<script>
+  let { size = "medium", image = null, username = "no user", colour = "--background-tertiary"} = $props();
+</script>
+
+<div class={size}>
+    {#if image}
+        <img class="image" src={image} alt={username}/>
+    {:else}
+        <div class="image" style={`background-color: var(${colour})`}></div>
+    {/if}
+</div>
+
+<style>
+.image {
+    width: 100%;
+    height: 100%;
+    left: 0px;
+    top: 0px;
+    position: absolute;
+    border-radius: 9999px;
+}
+
+.medium {
+    width: 18px;
+    height: 18px;
+    position: relative;
+}
+
+.large {
+    width: 32px;
+    height: 32px;
+    position: relative;
+}
+</style>
