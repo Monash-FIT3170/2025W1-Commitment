76b9254c13f3c600d8c476fc5f57f172215bfbdf
npnquang <nguyenjoe0201@gmail.com>
2025-08-13 13:14:43 +1000

    refactor: remove $lib/repo.ts
    combine $lib/repo.ts and $lib/stores/repo.ts together

--- src/lib/stores/repo.ts
+++ src/lib/stores/repo.ts
@@ -1,6 +1,31 @@
 import { writable } from "svelte/store";
-import type { Repo } from "$lib/repo";
-import { get_repo_type } from "$lib/repo";
+
+export type Repo = {
+    repo_name: string;
+    repo_url: string;
+    repo_type: string
+};
+
+export function get_repo_type(url: string) {
+    const domain = new URL(url).hostname;
+
+    if (domain.includes("github.com")) {
+        return "github";
+    } else if (domain.includes("gitlab.com")) {
+        return "gitlab";
+    } else {
+        return "Unknown";
+    }
+}
+
+export function get_repo_name(url: string) {
+    let result = new URL(url).pathname.split("/").at(-1);
+    if (result) {
+        return result;
+    } else {
+        return "Unknown";
+    }
+}
 
 export const current_repo = writable<Repo>();
 
