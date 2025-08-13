8dfa61e5df9509c928511a1d418f37cc32131a14
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-08-13 11:09:53 +1000

    conf: Added powershell script to setup git hooks

--- git-hooks.ps1
+++ git-hooks.ps1
@@ -1,17 +1,17 @@
 param (
-    [Alias('l', 'link')]
+    [Alias('l')]
     [switch]$Link,
     
-    [Alias('d', 'delink')]
+    [Alias('d')]
     [switch]$Delink
 )
 
-if ($Link) {
+if ($Link){
     # Create symbolic links for Git hooks
     New-Item -ItemType SymbolicLink -Path ".git/hooks/prepare-commit-msg" -Target "../../.githooks/prepare-commit-msg" -Force
     New-Item -ItemType SymbolicLink -Path ".git/hooks/pre-commit" -Target "../../.githooks/pre-commit" -Force
 }
-elseif ($Delink) {
+elseif ($Delink){
     # Remove Git hooks
     Remove-Item ".git/hooks/prepare-commit-msg" -ErrorAction SilentlyContinue
     Remove-Item ".git/hooks/pre-commit" -ErrorAction SilentlyContinue
@@ -19,11 +19,11 @@ elseif ($Delink) {
 else {
     # Display help information
     Write-Host @"
-PowerShell git-hooks.ps1 [[-l | --link] | [-d | --delink]]
+git-hooks.ps1 [[-l | -Link] | [-d | -Delink]]
 
 Sets up Git Hooks for project workflow.
 
---link   - Links Git hooks to hooks in .git-hooks directory
---delink - Unlinks it hooks, useful during a rebase
+-Link   - Links Git hooks to hooks in .git-hooks directory
+-Delink - Unlinks it hooks, useful during a rebase
 "@
 }
