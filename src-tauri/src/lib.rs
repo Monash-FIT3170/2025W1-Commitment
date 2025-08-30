e1387010a9429e34269a0d93cb2db61378c2beff
Georgia Kanellis <gkan0011@student.monash.edu>
2025-08-30 17:34:45 +1000

    feat: added downlaod dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,11 +3,14 @@ mod contributor;
 mod repositories;
 mod url_verifier;
 
+// use tauri_plugin_fs;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             branches::get_branch_names,
             contributor::get_contributor_info,
