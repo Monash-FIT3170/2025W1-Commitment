d8d9ada049a138d03df79dce5bbd167a3777ece2
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-10-10 18:10:33 +1100

    fix: Default branch is selected based on HEAD

--- src-tauri/src/branches.rs
+++ src-tauri/src/branches.rs
@@ -1,4 +1,5 @@
 use git2::Repository;
+use log::info;
 
 #[tauri::command(rename_all = "snake_case")]
 pub async fn get_branch_names(path: &str) -> Result<Vec<String>, String> {
@@ -11,8 +12,12 @@ pub async fn get_branch_names(path: &str) -> Result<Vec<String>, String> {
     };
 
     let repo = Repository::open(canonical_path).map_err(|e| e.to_string())?;
+    let head = repo.head().map_err(|e| e.to_string())?;
+    let head_name = head.shorthand();
 
-    let branches = repo
+    info!("head: {}", head_name.unwrap());
+
+    let mut branches = repo
         .branches(None)
         .map_err(|e| e.to_string())?
         .map(|b| {
@@ -25,6 +30,17 @@ pub async fn get_branch_names(path: &str) -> Result<Vec<String>, String> {
                 .to_string()
         })
         .collect::<Vec<String>>();
+
+    if let Some(head_str) = head_name {
+        branches
+            .iter()
+            .position(|b| b.eq(head_str))
+            .iter()
+            .for_each(|head_idx| {
+                branches.remove(*head_idx);
+                branches.insert(0, head_str.to_string());
+            });
+    }
 
     Ok(branches)
 }
