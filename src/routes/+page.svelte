f9f4ad8b0b3a9d5382a5e9503e205715cea97a62
MassimoNodin <mnod0001@student.monash.edu>
2025-10-21 01:29:07 +1100

    feat: integrate loading_sleep utility
    - Implement loading_sleep in sidebar and main page to ensure consistent loading times

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,6 +23,7 @@
     import { manifest, type ManifestSchema } from "$lib/stores/manifest";
     import { info, error } from "@tauri-apps/plugin-log";
     import LoadingIndicator from "$lib/components/global/LoadingIndicator.svelte";
+    import { loading_sleep } from "$lib/utils/sleep";
 
     // only run on the browser
     onMount(async () => {
@@ -168,7 +169,7 @@
 
     async function handle_verification() {
         loading = true;
-
+        let start_time = Date.now();
         info(
             "handleVerification called with: " + repo_url_input + " " + selected
         );
@@ -180,8 +181,6 @@
             loading = false;
             return;
         }
-
-        loading = true;
 
         let source_type = get_source_type(repo_url_input);
 
@@ -200,6 +199,7 @@
                 verification_error = true;
                 verification_message =
                     "Please enter a valid URL/path. (Prefix with https:// or /)";
+                await loading_sleep(start_time);
                 loading = false;
                 return;
             }
@@ -257,6 +257,7 @@
                         verification_message = "Unknown Error: " + err_check;
                     }
                     verification_error = true;
+                    await loading_sleep(start_time);
                     loading = false;
                     return;
                 }
@@ -306,6 +307,8 @@
             await save_state(storage_obj);
 
             // Navigate to the overview page
+            await loading_sleep(start_time);
+            loading = false;
             goto(`/overview-page`);
         } catch (error: any) {
             loading = false;
